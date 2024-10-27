9c6ccda137f50ccf391d1c0593727d3bcaf96489
sammywasukundi <brosammy1et2@gmail.com>
2024-10-27 21:07:49 +0100

    mise en place du crud frontend pour les tables fourniture et sa reference

--- pages/stock.php
+++ pages/stock.php
@@ -56,7 +56,7 @@ require 'sidebar.php';
 
 
                     </div>
-                    <form action="#" method="post" enctype="multipart/form-data" class="mt-6">
+                    <form action="stock.php" method="post" enctype="multipart/form-data" class="mt-6">
                         <div class="grid gap-4 mb-4 grid-cols-2">
                             <div class="col-span-2 sm:col-span-1">
                                 <label for="idRefacquisition"
@@ -186,7 +186,7 @@ require 'sidebar.php';
                     <div class="flex justify-between">
                         <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Ajouter une fourniture</h2>
                         <div>
-                            <button data-modal-target="crud-modal" data-modal-toggle="crud-modal"
+                            <button data-modal-target="modal-ref-fourniture" data-modal-toggle="modal-ref-fourniture"
                                 class="block text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
                                 type="button">
                                 Ajouter une réference
@@ -196,57 +196,84 @@ require 'sidebar.php';
 
 
                     </div>
-                    <form action="#" method="post" enctype="multipart/form-data">
-                        <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
-                            <div class="md:block hidden">
-                                <img class="object-cover" src="../assets/vectors/department.jpg" alt="">
+                    <form action="stock.php" method="post" enctype="multipart/form-data">
+                        <div class="grid gap-4 mb-4 grid-cols-2">
+                            <div class="col-span-2 sm:col-span-1">
+                                <label for="idRefacquisition"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sélectionnez
+                                    une réference de fourniture</label>
+                                <select id="idReffourniture" name="idReffourniture"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                    <option value="TV">Ref fourniture</option>
+                                    <option value="PC">Ref fourniture</option>
+                                    <option value="GA">fourniture</option>
+                                </select>
                             </div>
-                            <div class="block">
-                                <div class="w-full">
-                                    <label for="Designation"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Designation</label>
-                                    <input type="text" name="Designation" id="Designation"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                        placeholder="millionnaire" required="">
-                                </div>
-                                <div class="w-full">
-                                    <label for="Responsable"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Responsable</label>
-                                    <input type="text" name="Responsable" id="Responsable"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                        placeholder="millionnaire" required="">
-                                </div>
-                                <div class="w-full">
-                                    <label for="email"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email du
-                                        fournisseur</label>
-                                    <input type="text" name="adresse_fournisseur" id="adresse_fournisseur"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                        placeholder="fournisseur@gmail.com" required="">
-                                </div>
-                                <button name="submit_supplier" type="submit"
-                                    class="items-center px-3 py-2 mt-4 w-full sm:mt-6 text-sm font-medium text-center text-white rounded-md bg-green-500 hover:bg-green-600">
-                                    Ajouter le departement
-                                </button>
+                            <div class="col-span-2 sm:col-span-1">
+                                <label for="idProduit"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sélectionnez un
+                                    produit</label>
+                                <select id="idProduit" name="idProduit"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                    <option value="TV">Jeanpie</option>
+                                    <option value="PC">Sammy</option>
+                                    <option value="GA">Jp/Console</option>
+                                </select>
+                            </div>
+                            <div class="col-span-2 sm:col-span-1">
+                                <label for="quantite"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quantité</label>
+                                <input type="number" name="quantite" id="quantite"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                    required="">
+                            </div>
+                            <div class="col-span-2 sm:col-span-1">
+                                <label for="uniteQte"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">unité de la
+                                    quantité</label>
+                                <input type="number" name="uniteQte" id="uniteQte"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                                    required="">
+                            </div>
+                            <div class="col-span-2 sm:col-span-1">
+                                <label for="idRefacquisition"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sélectionnez
+                                    un departement</label>
+                                <select id="idDepartement" name="idDepartement"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                    <option value="TV">Ref departement</option>
+                                    <option value="PC">Ref departement</option>
+                                    <option value="GA">departement</option>
+                                </select>
                             </div>
                         </div>
+                        <button name="submit_fourniture" type="submit"
+                            class="items-center px-3 py-2 mt-4 w-full sm:mt-6 text-sm font-medium text-center text-white rounded-md bg-green-500 hover:bg-green-600">
+                            Ajouter la fourniture
+                        </button>
                     </form>
 
 
                     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 mt-6">
                             <thead
                                 class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
                                     <th scope="col" class="px-6 py-3">
-                                        Designation
+                                        Réference de fourniture
                                     </th>
                                     <th scope="col" class="px-6 py-3">
-                                        Responsable
+                                        Produit
                                     </th>
                                     <th scope="col" class="px-6 py-3">
-                                        Email du
-                                        fournisseur
+                                        Quantité
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        unité de la
+                                        quantité
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Departement
                                     </th>
                                     <th scope="col" class="px-6 py-3">
                                         Action
@@ -262,6 +289,12 @@ require 'sidebar.php';
                                         class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                         Apple MacBook Pro 17"
                                     </th>
+                                    <td class="px-6 py-4">
+                                        Silver
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        Laptop
+                                    </td>
                                     <td class="px-6 py-4">
                                         Silver
                                     </td>
@@ -303,7 +336,7 @@ require 'sidebar.php';
 
 
 
-<!-- Main modal pour ajouter une réference-->
+<!-- Main modal pour ajouter une réference a une acquisition-->
 <div id="crud-modal" tabindex="-1" aria-hidden="true"
     class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-md max-h-full">
@@ -362,4 +395,65 @@ require 'sidebar.php';
         </div>
     </div>
 </div>
-<!-- mon modal pour la réference ici -->
+<!-- mon modal pour la réference de l'acquisition ici -->
+
+
+<!-- Main modal pour ajouter une réference a une fourniture-->
+<div id="modal-ref-fourniture" tabindex="-1" aria-hidden="true"
+    class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-md max-h-full">
+        <!-- Modal content -->
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                    Ajouter une réference
+                </h3>
+                <button type="button"
+                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 bg-green-500 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    data-modal-toggle="modal-ref-fourniture">
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                        viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                    </svg>
+                    <span class="sr-only">Close modal</span>
+                </button>
+            </div>
+            <!-- Modal body -->
+            <form class="p-4 md:p-5" method="post" action="stock.php">
+                <div class="grid gap-4 mb-4 grid-cols-2">
+                    <div class="col-span-2 sm:col-span-1">
+                        <label for="category"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Sélectionnez un
+                            Etat de besoin</label>
+                        <select id="idEtat_besoin" name="idEtat_besoin"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            <option value="TV">Omo</option>
+                            <option value="PC">Biscuit</option>
+                            <option value="GA">Console</option>
+                        </select>
+                    </div>
+                    <div class="col-span-2 sm:col-span-1">
+                        <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date de
+                            réference de fourniture</label>
+                        <input type="date" name="DateFourniture" id="DateFourniture"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Ecrivez la date ici" required="">
+                    </div>
+                </div>
+                <button type="submit" name="submitRefFourniture"
+                    class="text-white inline-flex items-center bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                    <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                    Ajouter une réference
+                </button>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- mon modal pour la réference a une fourniture ici -->
